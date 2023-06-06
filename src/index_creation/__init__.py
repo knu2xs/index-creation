6fa028bbb944cab7d5a498bbce93cb0dd0902a0c
Joel McCune <knu2xs@gmail.com>
2023-06-06 07:01:09 -0700

    Bump version: 0.1.0.dev0 → 0.1.0

--- src/index_creation/__init__.py
+++ src/index_creation/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'index-creation'
-__version__ = '0.1.0.dev0'
+__version__ = '0.1.0'
 __author__ = 'Joel McCune'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2023 by Joel McCune'
