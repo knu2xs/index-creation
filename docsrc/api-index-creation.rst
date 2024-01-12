2d7e0d1828ef16ca8a7eb62eec618227667c255d
Joel McCune <knu2xs@gmail.com>
2024-01-12 10:01:30 -0800

    updates for streamlining documentation and package code

---
+++ docsrc/api-index-creation.rst
@@ -0,0 +1,11 @@
+``index_creation``
+==================
+
+.. automodule:: index_creation
+    :members:
+
+``index_creation.arcgis``
+-------------------------
+
+.. automodule:: index_creation.arcgis
+   :members:
--- src/index_creation/arcgis.py
+++ src/index_creation/arcgis.py
@@ -1,3 +1,4 @@
+import logging
 from functools import lru_cache
 from pathlib import Path
 import pkgutil
@@ -5,15 +6,16 @@ from tempfile import gettempdir
 from typing import Union, List
 
 from .. import config
-from .. import get_simpsons_diversity_index
+from ..stats import get_simpsons_diversity_index
 
 __all__ = [
-    'add_simpsons_diversity_index_feature_class',
-    'add_preconfigured_simpsons_diversity_index_feature_class'
+    "add_simpsons_diversity_index_feature_class",
+    "add_preconfigured_simpsons_diversity_index_feature_class",
 ]
 
 # provide variable indicating if arcpy is available
-has_arcpy: bool = pkgutil.find_loader('arcpy') is not None
+has_arcpy: bool = pkgutil.find_loader("arcpy") is not None
+
 
 @lru_cache(16)
 def get_temp_gdb() -> Path:
@@ -25,8 +27,7 @@ def get_temp_gdb() -> Path:
 
     # create a temporary file geodatabase to store data
     gdb = arcpy.management.CreateFileGDB(
-        out_folder_path=gettempdir(),
-        out_name='tmpfgdb.gdb'
+        out_folder_path=gettempdir(), out_name="tmpfgdb.gdb"
     )[0]
 
     # convert the path to a Path object instance
@@ -36,9 +37,9 @@ def get_temp_gdb() -> Path:
 
 
 def add_simpsons_diversity_index_feature_class(
-        data: Union[str, Path, 'arcpy._mp.Layer'],
-        input_fields: List[str],
-        simpson_diversity_index_field: str = 'simpson_diversity_index'
+    data: Union[str, Path, "arcpy._mp.Layer"],
+    input_fields: List[str],
+    simpson_diversity_index_field: str = "simpson_diversity_index",
 ) -> Path:
     """
     Add a new column with Simpson's Diversity Index calculated from existing columns already existing in the dataset.
@@ -56,7 +57,9 @@ def add_simpsons_diversity_index_feature_class(
     """
     # ensure arcpy is available
     if not has_arcpy:
-        raise EnvironmentError('ArcPy is required to execute the add_simpsons_diversity_index_feature_class function.')
+        raise EnvironmentError(
+            "ArcPy is required to execute the add_simpsons_diversity_index_feature_class function."
+        )
     else:
         import arcpy
 
@@ -68,20 +71,28 @@ def add_simpsons_diversity_index_feature_class(
 
     # check to ensure the column to be added, does not already exist
     if simpson_diversity_index_field in obs_flds:
-        raise ValueError(f"It appears the field to be added for the Simpson's Diversity Index, "
-                         f"{simpson_diversity_index_field}, already exists in the Feature Class attribute table.")
+        raise ValueError(
+            f"It appears the field to be added for the Simpson's Diversity Index, "
+            f"{simpson_diversity_index_field}, already exists in the Feature Class attribute table."
+        )
 
     # ensure the source fields exist in the feature class
     missing_cols = [c for c in input_fields if c not in obs_flds]
     if len(missing_cols):
-        raise ValueError(f'Cannot locate all input_fields. Not detecting the following fields in the Feature Class - '
-                         f'{missing_cols}.')
+        raise ValueError(
+            f"Cannot locate all input_fields. Not detecting the following fields in the Feature Class - "
+            f"{missing_cols}."
+        )
 
     # add a new field for the new index
-    arcpy.management.AddField(data, field_name=simpson_diversity_index_field, field_type='FLOAT')
+    arcpy.management.AddField(
+        data, field_name=simpson_diversity_index_field, field_type="FLOAT"
+    )
 
     # create an update cursor for index calculation
-    with arcpy.da.UpdateCursor(data, input_fields + [simpson_diversity_index_field]) as update_cursor:
+    with arcpy.da.UpdateCursor(
+        data, input_fields + [simpson_diversity_index_field]
+    ) as update_cursor:
 
         # iterate the rows
         for r in update_cursor:
@@ -99,16 +110,28 @@ def add_simpsons_diversity_index_feature_class(
 
 
 def add_preconfigured_simpsons_diversity_index_feature_class(
-        input_features: Union[str, Path],
-        index_name: str,
-        include_enrich_fields: bool = False
+    input_features: Union[str, Path],
+    index_name: str,
+    include_enrich_fields: bool = False,
 ) -> Union[Path, str]:
     """
     Add a preconfigured Simpson's Diversity Index to a Feature Class using one of the preconfigured indices.
+
+    Currently, the preconfigured indices include:
+
+    - ``income``
+    - ``home_value``
+    - ``wealth``
+    - ``age``
+    - ``home_age``
+    - ``employment_diversity``
+    - ``housing_diversity``
+
     Args:
-        input_features:
-        index_name:
-        include_enrich_fields:
+        input_features: Feature Class to use as input for calculating Simpson's Diversity Index.
+        index_name: Name of one of the available preconfigured indices.
+        include_enrich_fields: Whether to leave the source fields for calculating the index. These fields are
+            added using enrichment. The default is false, which removes these fields added through enrichment.
     """
     # late import
     import arcpy
@@ -124,33 +147,37 @@ def add_preconfigured_simpsons_diversity_index_feature_class(
     # ensure the requested index is available
     idx_names = config.meta_variables.keys()
     if index_name not in idx_names:
-        raise ValueError(f'Unfortunately, your requested index, "{index_name}," is not available. Please select '
-                         f'from {idx_names}')
+        raise ValueError(
+            f'Unfortunately, your requested index, "{index_name}," is not available. Please select '
+            f"from {idx_names}"
+        )
 
     # get the enrich variable names from the config
     var_name_lst = config.meta_variables[index_name]
 
     # create the country object for looking up values
-    cntry = Country('USA', 'local')
+    cntry = Country("USA", "local")
 
     # get the enrich strings and output field names to use
-    ev_df = cntry.enrich_variables[cntry.enrich_variables['name'].isin(var_name_lst)]
-    enrich_str_lst = list(ev_df['enrich_name'])
-    enrich_fld_lst = list(ev_df['enrich_field_name'])
-    enrich_str = ';'.join(enrich_str_lst)
+    ev_df = cntry.enrich_variables[cntry.enrich_variables["name"].isin(var_name_lst)]
+    enrich_str_lst = list(ev_df["enrich_name"])
+    enrich_fld_lst = list(ev_df["enrich_field_name"])
+    enrich_str = ";".join(enrich_str_lst)
 
     # enrich the input data and save to a temporary output feature class
     enrich_fc = arcpy.ba.EnrichLayer(
         in_features=input_features,
-        out_feature_class=str(tmp_gdb / 'temp_enrich'),
-        variables=enrich_str
+        out_feature_class=str(tmp_gdb / "temp_enrich"),
+        variables=enrich_str,
     )[0]
 
     # add the simpson index to the output fields
-    simpson_field_name = f'simpson_diversity_index_{index_name}'
+    simpson_field_name = f"simpson_diversity_index_{index_name}"
 
     # add the simpsons diversity index field onto the temp feature class
-    add_simpsons_diversity_index_feature_class(enrich_fc, enrich_fld_lst, simpson_field_name)
+    add_simpsons_diversity_index_feature_class(
+        enrich_fc, enrich_fld_lst, simpson_field_name
+    )
 
     # create a list of fields to add...taking into account if adding the enrich fields or not
     if include_enrich_fields:
@@ -185,5 +212,10 @@ def add_preconfigured_simpsons_diversity_index_feature_class(
 
             # push the update to the input table
             update_cur.updateRow(enrich_row)
+
+    # provide status feedback
+    logging.info(
+        f"Calculated Simson's Diversity Index named {simpson_field_name} using {var_name_lst}"
+    )
 
     return input_features
